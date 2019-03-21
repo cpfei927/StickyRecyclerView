60e2bb6839260df678ce0360370f5e9b9a235be8
chengpengfei <chengpengfei@100tal.com>
2019-03-21 16:25:45 +0800

    添加demo apk

--- app/src/main/java/com/cpfei/stickyrecyclerview/StickyOnScrollListener.java
+++ app/src/main/java/com/cpfei/stickyrecyclerview/StickyOnScrollListener.java
@@ -7,7 +7,6 @@ import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.RelativeLayout;
-import android.widget.TextView;
 
 /**
  * Created by cpfei on 2019/3/21
